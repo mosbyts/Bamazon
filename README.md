979df172e42fab87d1ca6beb73b7d73ad95dbf07
Tianna Mosby <mosbyts@vcu.edu>
2019-10-29 18:20:07 -0400

    Updated game, fully working.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Bamazon
+
+## Description
+
+Bamazon is the low-budget version of Amazon that can be used to pretend to purchase common items.
+
+## Motivation
+This tool was created as part of the University of Richmond Full Stack Flex Web Development program curriculum to further databasing skills using mySQL. It is useful for those who want to learn more about an artist's next concert, a movie or a song.
+
+## Tech Used
+- JavaScript
+- jQuery
+- JSON Packages (mysql & inquirer)
+- mySQL
+This tool was created using a JavaScript file to pull information from the JSON packages and display it to the user then use inquirer to receive information on what the user would want from the premade items in the database.
+
+## For Developers
+Clone the game locally:
+    $ git clone https://github.com/mosbyts/bamazon.git
+
+## Credit
+This version of the game was created by Tianna Mosby. For questions, concerns or additional help please contact me on GitHub at mosbyts.
+
+## License
+None.
+
+## Example Picture
+See "example-picture" in repository.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -56,9 +56,19 @@ function placeOrder(){
                 connection.query(("UPDATE products SET stock_quantity = " + updateStock +  " WHERE item_id = " + parseInt(userAnswer.productID)), function(err, res) {
                         if (err) throw err;
                         var calculatePrice = productResponse[0].price * userAnswer.productUnits;
-                        console.log("Order placed. Your item totaled to $" + calculatePrice);
-                        connection.end();
-                    });
+                        console.log("Your item has totaled to $" + calculatePrice);
+                        inquirer.prompt([
+                            {
+                                type: "list",
+                                name: "payment",
+                                message: "How would you like to pay today?",
+                                choices: ["Credit Card", "Debit Card", "Apple Pay"]
+                            },
+                        ]).then(function(paymentMethod){
+                            console.log("You have picked " + paymentMethod.payment + ". Your order has been placed. Please expect it to arrive within 5-7 business days. Goodbye!")
+                        });
+                    connection.end();
+                });
             };
         });
     })};
